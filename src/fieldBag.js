6384a9cb9ad6369d7557a316e9db49c2999cd1c1
Johann Troendle <johann.troendle@gmail.com>
2016-10-18 13:12:31 -0400

    Removed Proxy()

--- src/fieldBag.js
+++ src/fieldBag.js
@@ -2,23 +2,29 @@ export default class FieldBag {
     constructor($vm) {
         this.$vm = $vm;
         // Needed to bypass render errors if the fields aren't populated yet.
-        this.fields = new Proxy({}, {
-            get(target, property) {
-                if (! (property in target) && typeof property === 'string') {
-                    // eslint-disable-next-line
-                    target[property] = {};
+        this.fields = (() => {
+            const _fields = {};
+
+            return (property, remove) => {
+                if (remove === true) {
+                    delete _fields[property];
+                    return;
                 }
 
-                return target[property];
-            }
-        });
+                if (!(property in _fields) && typeof property === 'string') {
+                    // eslint-disable-next-line
+                    _fields[property] = {};
+                }
+                // eslint-disable-next-line
+                return _fields[property];
+            };
+        })();
     }
 
     /**
      * Initializes and adds a new field to the bag.
      */
     _add(name) {
-        this.fields[name] = {};
         this._setFlags(name, { dirty: false, valid: false }, true);
     }
 
@@ -26,7 +32,7 @@ export default class FieldBag {
      * Remooves a field from the bag.
      */
     _remove(name) {
-        delete this.fields[name];
+        this.fields(name, true);
     }
 
     /**
@@ -57,18 +63,18 @@ export default class FieldBag {
      * Updates the dirty flag for a specified field with its dependant flags.
      */
     setDirty(name, value, initial = false) {
-        this.fields[name].dirty = value;
-        this.fields[name].clean = initial || ! value;
-        this.fields[name].passed = this.fields[name].valid && value;
-        this.fields[name].failed = ! this.fields[name].valid && value;
+        this.fields(name).dirty = value;
+        this.fields(name).clean = initial || !value;
+        this.fields(name).passed = this.fields(name).valid && value;
+        this.fields(name).failed = !this.fields(name).valid && value;
     }
 
     /**
      * Updates the valid flag for a specified field with its dependant flags.
      */
     setValid(name, value) {
-        this.fields[name].valid = value;
-        this.fields[name].passed = this.fields[name].dirty && value;
-        this.fields[name].failed = this.fields[name].dirty && ! value;
+        this.fields(name).valid = value;
+        this.fields(name).passed = this.fields(name).dirty && value;
+        this.fields(name).failed = this.fields(name).dirty && !value;
     }
 }
